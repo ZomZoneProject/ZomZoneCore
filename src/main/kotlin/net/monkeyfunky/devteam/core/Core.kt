38e4c768ebc975caffa77d9344c1b715966c0de3
eight_y_88 <jadec.eight@gmail.com>
2021-11-08 19:19:30 +0900

    Add Motd Change

--- src/main/kotlin/net/monkeyfunky/devteam/core/Core.kt
+++ src/main/kotlin/net/monkeyfunky/devteam/core/Core.kt
@@ -1,6 +1,7 @@
 package net.monkeyfunky.devteam.core
 
 import net.monkeyfunky.devteam.core.events.LogInOutListener
+import net.monkeyfunky.devteam.core.events.MotdChangeListener
 import org.bukkit.Bukkit
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
@@ -32,6 +33,7 @@ class Core : JavaPlugin() {
                 LogInOutListener.FIRST_JOIN_MESSAGE = config.getString("message.join.first").toString()
                 LogInOutListener.NORMAL_JOIN_MESSAGE = config.getString("message.join.normal").toString()
                 LogInOutListener.QUIT_MESSAGE = config.getString("message.left").toString()
+                MotdChangeListener.MOTD_LIST = config.getStringList("motd")
             }
         } catch (e: Exception) {
             println(e.message)
