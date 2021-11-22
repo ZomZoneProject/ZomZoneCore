91788db576b0bfc7ef3e6433d11378f0436707d9
eight_y_88 <jadec.eight@gmail.com>
2021-11-22 13:17:18 +0900

    Block some commands

--- src/main/kotlin/net/monkeyfunky/devteam/core/EventListener.kt
+++ src/main/kotlin/net/monkeyfunky/devteam/core/EventListener.kt
@@ -1,5 +1,21 @@
 package net.monkeyfunky.devteam.core
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
 
-class EventListener : Listener
+class EventListener : Listener {
+    private val block = listOf(
+        "/pl",
+        "/plugins",
+        "/help",
+        "/?"
+    )
+
+    @EventHandler
+    fun onCommand(e: PlayerCommandPreprocessEvent) {
+        if (block.any { e.message.contains(it) }) {
+            e.isCancelled = true
+        }
+    }
+}
