c38270463420de79a94f1a7e8b0ab2f6c9e57829
eight_y_88 <jadec.eight@gmail.com>
2021-12-13 17:26:10 +0900

    Log4j2(client side)

--- src/main/kotlin/net/monkeyfunky/devteam/core/EventListener.kt
+++ src/main/kotlin/net/monkeyfunky/devteam/core/EventListener.kt
@@ -2,9 +2,13 @@ package net.monkeyfunky.devteam.core
 
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
+import java.util.regex.Pattern
 
 class EventListener : Listener {
+    private val pattern: Pattern = Pattern.compile("\\$\\{jdni:([^}]*)}")
+
     private val blocked = listOf(
         "/pl",
         "/plugins",
@@ -16,6 +20,13 @@ class EventListener : Listener {
     fun onCommand(e: PlayerCommandPreprocessEvent) {
         if (blocked.any { e.message.startsWith(it) } && !e.player.isOp) {
             e.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onPlayerChat(e: AsyncPlayerChatEvent) {
+        if (pattern.matcher(e.message).find()) {
+            e.message = "<" + e.player.name + "> ***********************"
         }
     }
 }
